cf45d45bbc708de78f245a01dbc90dd1faa95c7a
huynguyenj <nguyenhuyjobs@gmail.com>
2026-03-17 20:13:52 +0700

    create: call api hook

--- src/features/auth/hooks/useLogin.tsx
+++ src/features/auth/hooks/useLogin.tsx
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { AuthApi } from '../api/auth-api'
 import { useAuthStore } from '../store/auth-store'
 import { useNavigate } from 'react-router'
 import { PRIVATE_PATH } from '@/router/path'
-import { toast } from 'react-toastify'
+import type { LoginResponseType } from '../types/login-types'
+import useApiCall from '@/config/useApiCall'
 
 const LoginFormSchema = z.object({
   username: z.email(),
@@ -17,16 +17,17 @@ export default function useLogin() {
   const { register, handleSubmit, formState: { errors } } = useForm<LoginFormType>({ resolver: zodResolver(LoginFormSchema) })
   const addAuthStore = useAuthStore((state) => state.setAccessToken)
   const navigate = useNavigate()
+  const { execute, loading } = useApiCall<LoginResponseType>()
   const onSubmit = async (formData: LoginFormType) => {
-    try {
-      const loginData = await AuthApi.login(formData)
-      const { accessToken, role, accountId, staffId } = loginData.data
-      addAuthStore(accessToken, accountId, staffId, role)
-      navigate(PRIVATE_PATH.CHAT)
-    } catch (error) {
-      console.log(error)
-      toast.error('Login fail')
-    }
+    const apiData = await execute({
+      apiUrl: '/auth/login',
+      method: 'post',
+      type: 'public',
+      body: formData
+    })
+    const { accessToken, role, accountId, staffId } = apiData.data
+    addAuthStore(accessToken, accountId, staffId, role)
+    navigate(PRIVATE_PATH.CHAT)
   }
-  return { register, handleSubmit, onSubmit, errors }
+  return { register, handleSubmit, onSubmit, errors, loading }
 }
--- src/pages/global/LoginPage.tsx
+++ src/pages/global/LoginPage.tsx
@@ -1,8 +1,9 @@
 import Logo from '@/assets/logo.jpg'
+import Button from '@/components/ui/button/Button'
 import Input from '@/components/ui/input/Input'
 import useLogin from '@/features/auth/hooks/useLogin'
 export default function LoginPage() {
-  const { errors, handleSubmit, onSubmit, register } = useLogin()
+  const { errors, handleSubmit, onSubmit, register, loading } = useLogin()
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
       <div className='w-100 md:w-[60] xl:w-120 min-w-90 px-10 py-12 shadow-[0px_5px_10px_1px_rgba(0,0,0,0.2)] rounded-2xl'>
@@ -16,7 +17,9 @@ export default function LoginPage() {
             <Input {...register('username')} type='text' variant='gray' label='Email' placeholder='example@company.com' error={errors.username?.message}/>
             <Input {...register('password')} type='password' variant='gray' label='Mật khẩu' placeholder='123456789' error={errors.password?.message}/>
           </div>
-          <button className='py-2 px-3 text-center bg-secondary rounded-lg text-white font-semibold w-full hover:bg-secondary-hover cursor-pointer'>Đăng nhập</button>
+          <Button variant='default' className={`w-full ${loading && 'bg-soft-gray text-white'}`} disabled={loading ? true: false}>
+            Đăng nhập
+          </Button>
         </form>
       </div>
     </div>
