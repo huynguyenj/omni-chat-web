26bcf7569034755e4fadbba2eea72bb4edcc70b3
thong minh <minh35940@gmail.com>
2026-03-20 10:49:15 +0700

    Add comment for MainNavber

--- src/components/navigation/MainNavbar.tsx
+++ src/components/navigation/MainNavbar.tsx
@@ -7,11 +7,14 @@ export default function MainNavbar() {
   return (
      <div className="sticky top-0 z-20 flex items-center justify-between w-full h-20 bg-primary px-5">
       <img src={Logo} alt="Logo" loading='lazy' className='w-10 h-10' />
+     
+     {/* nav cho home, admin và manager */}
       <nav className="flex items-center gap-6">
         <a href="/" className="text-white hover:underline">Home</a>
         <a href="/admin" className="text-white hover:underline">Admin</a>
         <a href="/manager" className="text-white hover:underline">Manager</a>
       </nav>
+     
       <div className='flex items-center justify-between gap-8'>
         <FiBell className='w-6 h-6 text-white'/>
         <div className='h-15 w-[0.1rem] bg-white'></div>
