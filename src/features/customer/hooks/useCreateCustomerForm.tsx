203216f18fcb13eb6d0aa0b8e86710be29257498
huynguyenj <nguyenhuyjobs@gmail.com>
2026-04-28 20:36:41 +0700

    update api to customer form page

--- src/features/customer/hooks/useCreateCustomerForm.tsx
+++ src/features/customer/hooks/useCreateCustomerForm.tsx
@@ -3,25 +3,30 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
 import z from 'zod'
+import type { FormCustomerType } from '../types/form-type'
 
 const customerFormSchema = z.object({
-  name: z.string({ error: 'Tên không đươc để trống' }),
-  phone: z.number({ error: 'Số điện thoại không được để trống' }).max(12, { error: 'Số điện thoại không được quá 12 số' }),
+  address: z.string({ error: 'Địa chỉ không được để trống' }),
+  phone: z.string({ error: 'Số điện thoại không được để trống' }).max(12, { error: 'Số điện thoại không được quá 12 số' }),
   email: z.email({ error: 'Hãy nhập đúng định dạng của email @gmail.com' })
 })
 
 type CustomerFormType = z.infer<typeof customerFormSchema>
 
-export default function useCreateCustomerForm({ conversationId }: { conversationId?: string }) {
+export default function useCreateCustomerForm({ activeCustomerId }: { activeCustomerId?: string }) {
   const { formState: { errors }, handleSubmit, register } = useForm<CustomerFormType>({ resolver: zodResolver(customerFormSchema) })
   const { execute, loading } = useApiCall<null>()
   const onSubmit = async (formData: CustomerFormType) => {
-    if (!conversationId) return
+    if (!activeCustomerId) return
+    const finalForm: FormCustomerType = {
+      ...formData,
+      activeCustomerId: activeCustomerId
+    }
     const apiData = await execute({
       apiUrl: '',
       method: 'post',
       type: 'public',
-      body: formData
+      body: finalForm
     })
     if (apiData.error) toast.error('Tạo thông tin thất bại')
   }
